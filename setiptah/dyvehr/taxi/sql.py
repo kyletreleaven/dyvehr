36808afa6c923701f979022e735977784d1a7396
Kyle Treleaven <treleaven.kyle@gmail.com>
2014-08-14 11:40:08 -0400

    just data schema comments

--- setiptah/dyvehr/taxi/sql.py
+++ setiptah/dyvehr/taxi/sql.py
@@ -1,1 +1,53 @@
 
+
+
+
+"""
+Schema should include:
+Experiment
+    -ID            ID
+    -arrivalrate    real
+    -numveh        int
+    -vehspeed        real
+    -distrib_id    ID
+    -utilization    real    (redundant)
+    -policy_id    ID
+    #
+    -init_dur    real
+    -time_factor    real
+    -thresh_factor    real
+    -exploit_ratio    real
+    
+Distribution
+    -ID            ID
+    -descr        text
+    -meancarry    real
+    -meanfetch    real
+    -moverscplx    real    (redundant)
+
+
+Policies
+    -ID            ID
+    -descr        text
+    
+    
+Demands
+    -local_id          ID
+    -experiment_id    ID
+    PRIMARY KEY (experiment_id,local_id)
+    #
+    -arrivaltime        real
+    -embarktime        real
+    -deliverytime    real
+    -waitdur        real    (redundant)
+    -carrydur        real (redundant)
+    -systemdur        real (redundant)
+    
+*We can manage this with a gui, yeah?
+"""
+
+
+
+
+
+
